177d4c2af8673daa496d32cfbb5a91c481af375b
Sekiguchi-Takashi <Sekiguchi-Takashi@users.noreply.github.com>
2026-07-29 06:22:42 +0900

    v1.0 fix AlertDialogのimport追加

--- app/src/main/java/com/appathy/mendan/MainActivity.kt
+++ app/src/main/java/com/appathy/mendan/MainActivity.kt
@@ -2,6 +2,7 @@ package com.appathy.mendan
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
